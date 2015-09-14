9e1439c9bbc7a1aa7f16bff515667c4774b90255
Chad Goymer <chad.goymer@gmail.com>
2015-09-14 17:02:35 +0100

    end of workshop

---
+++ app.R
@@ -0,0 +1,45 @@
+library(shiny)
+library(ggplot2)
+
+ui <- fluidPage(
+  # *Input() functions
+  fluidRow(
+    column(width = 4, offset = 4, 
+           sliderInput(inputId = "num", label = "Choose a number", min = 0, max = 100, value = 25)
+    )
+  ),
+  
+  # *Output() functions
+  fluidRow(
+    column(width = 6, 
+           plotOutput("hist")
+    ), 
+    column(width = 6, 
+           plotOutput("qq")
+    )
+  )
+  
+  # layout functions
+  
+)
+
+server <- function(input, output) {
+  # reactive() components
+  data <- reactive({rnorm(input$num)})
+  
+  # render*() functions (save results to ouput$)
+  output$hist <- renderPlot({
+    qplot(data(), geom = "histogram", xlab = "sample")
+    #     hist(rnorm(input$num))
+  })
+  
+  output$qq <- renderPlot({
+    qplot(sample = data(), stat = "qq")
+    #     qqnorm(rnorm(input$num))
+  })
+  
+  # observeEvent(), reactive(), eventReactive() functions
+  
+}
+
+shinyApp(server = server, ui = ui)
---
+++ exercises.R
@@ -0,0 +1,14 @@
+install.packages("reportsWS", repos = c("http://cran.rstudio.com", "http://training.rstudio.com"))
+
+setwd("R/projects/shiny-workshop")
+
+library(reportsWS)
+library(DT)
+# library(kable)
+library(rmarkdown)
+
+exercise(2)
+exercise(19)
+exercise(20)
+
+# check out shiny handsontable
